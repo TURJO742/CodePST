00d5055e99c61130da5fc4c2500e37039aec3347
Pritom Saha Turjo <159929161+TURJO742@users.noreply.github.com>
2024-03-15 22:07:06 +0600

    Function1.c
    Function first code

---
+++ Function1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void sum (int a,int b,int c)
+{
+    printf("The sum is %d\n",a+b+c);
+}
+void sub (int a,int b)
+{
+    printf("The sub is %d\n",a+b);
+}
+int main()
+{
+    sum(10,20,30);
+    sub(20,30);
+}
