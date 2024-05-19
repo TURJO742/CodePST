6e375c6f3fd41576be7ca589bb6c738e64ed70e7
Pritom Saha Turjo <159929161+TURJO742@users.noreply.github.com>
2024-05-20 01:26:53 +0600

    Add files via upload

---
+++ Bee_1011.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    double radius, volume;
+    const double pi = 3.14159;
+
+    scanf("%lf", &radius);
+
+    volume = (4.0 / 3.0) * pi * pow(radius, 3);
+
+    printf("VOLUME = %.3lf\n", volume);
+
+    return 0;
+}
